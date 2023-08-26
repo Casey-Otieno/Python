db75da3bee1dbf1ef001ae5af6b62787cf8c7ee9
Casey Otieno <77282162+Casey-Otieno@users.noreply.github.com>
2023-08-26 12:29:49 +0300

    Add files via upload

---
+++ basicPython.py
@@ -0,0 +1,28 @@
+print("Hello World")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+#variables and datatypes
+chr_name="Tom"
+chr_age="50"
+is_male= True
+print("The manager's name is "+ chr_name + "and he is"+ chr_age + "years old") # concatenation
+
+#Examples of functions for strings
+phrase= "Giraffe Academy"
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(len(phrase))
+print(phrase[4])
+print(phrase[0])
+print(phrase.index("f"))
+print(phrase.replace("Giraffe", "Elephant"))
+
+#Numbers
+print(2)
+#basic arithmetics
+print(3+4.5)
